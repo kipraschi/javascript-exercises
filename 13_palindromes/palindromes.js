56aa6392aa5fd7a7cf482792c598915c57ed7042
Kipras Chi <kipraschi@gmail.com>
2025-12-23 19:37:10 +0200

    feat: add function that checks if a string is palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanString = str
+        .toLowerCase()
+        .split('')
+        .filter(char => /[a-z0-9]/.test(char))
+        .join('');
 
+    let reversedString = cleanString.split('').reverse().join('');
+
+    return cleanString === reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
