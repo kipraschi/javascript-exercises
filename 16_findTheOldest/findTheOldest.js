58ec350fc0adc2af5e4497892e1022832b1cdc88
Kipras Chi <kipraschi@gmail.com>
2025-12-23 21:35:48 +0200

    feat: create a function that returns the oldest person from an array of objects

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    function getAge(birth, death) {
+        return death ? death - birth : new Date().getFullYear() - birth;
+    }
+
+    return people.reduce((oldest, person) => {
+        const currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        return currentAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
